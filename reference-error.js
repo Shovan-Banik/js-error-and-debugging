bc2fddf24e6cd02bfcc89a4a89da26de71eb6004
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-03-06 17:49:36 +0600

    reference error handling

---
+++ reference-error.js
@@ -0,0 +1,17 @@
+const x='pori';
+const y='moni';
+// console.log(z); /* ReferenceError: z is not defined */
+
+
+// console.log(name);/* ReferenceError: Cannot access 'name' before initialization */
+const name='gulguli molla';
+
+
+// console.log(add(2.3)); /* ReferenceError: Cannot access 'add' before initialization */
+const add=(a,b)=>a+b;
+
+
+function doSomething(){
+    const sum=23;
+}
+console.log(sum);/* ReferenceError: sum is not defined */
