4848a6f436a09d3a1109eac5f15ff7b52fa4a8af
Shovan Banik <53087831+Shovan-Banik@users.noreply.github.com>
2023-03-06 17:22:02 +0600

    understanding about syntax error

---
+++ syntax-errors.js
@@ -0,0 +1,9 @@
+const sum=(a,b)=>a+b;
+// const sum=a,b=>a+b; /* SyntaxError: Missing initializer in const declaration */
+const res=sum(3,5);
+console.log(res);
+
+// for(let i=0;i<10;i+){ /* SyntaxError: Unexpected token ')' */
+for(let i=0;i<10;i++){
+    console.log(i);
+}
